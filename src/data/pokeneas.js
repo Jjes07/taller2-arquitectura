3323247aadc8065a1acdb9b6fed08a16199afa57
Jjes07 <jjescobars@eafit.edu.co>
2026-05-08 17:16:19 -0500

    Added common structure for pokeneas in data

---
+++ src/data/pokeneas.js
@@ -0,0 +1,61 @@
+
+const pokeneas = [
+  {
+    id: 1,
+    name: "",
+    height: 0.0,
+    ability: "",
+    image: "",
+    quote: ""
+  },
+  {
+    id: 2,
+    name: "",
+    height: 0.0,
+    ability: "",
+    image: "",
+    quote: ""
+  },
+  {
+    id: 3,
+    name: "",
+    height: 0.0,
+    ability: "",
+    image: "",
+    quote: ""
+  },
+  {
+    id: 4,
+    name: "",
+    height: 0.0,
+    ability: "",
+    image: "",
+    quote: ""
+  },
+  {
+    id: 5,
+    name: "",
+    height: 0.0,
+    ability: "",
+    image: "",
+    quote: ""
+  },
+  {
+    id: 6,
+    name: "",
+    height: 0.0,
+    ability: "",
+    image: "",
+    quote: ""
+  },
+  {
+    id: 7,
+    name: "",
+    height: 0.0,
+    ability: "",
+    image: "",
+    quote: ""
+  },
+];
+
+module.exports = pokeneas;
